ffd34786edaa8ada16c5d05a10c2df313518a250
Abhjit_Baral <70994821+AbhijitBaral@users.noreply.github.com>
2024-03-13 00:21:01 +0530

    Update main.ino

--- 7 Segment Display/main.ino
+++ 7 Segment Display/main.ino
@@ -8,6 +8,7 @@ void setup() {
   pinMode(8,OUTPUT);  //g
 
   Serial.begin(9600);
+  while(!Serial){;}
 }
 //      digtlpin -   2 3 4 5 6 7 8
 //      displaypin   a b c d e f g
@@ -20,26 +21,31 @@ int segment[10][7]={{1,1,1,1,1,1,0},  //0
                     {1,0,1,1,1,1,1},  //6
                     {1,1,1,0,0,0,0},  //7
                     {1,1,1,1,1,1,1},  //8
-                    {1,1,1,1,1,0,1}   //9
+                    {1,1,1,1,0,1,1}   //9
                    };
+
+
 char ch;
-
-
+int x=0;
 void loop() {
+  
   if(Serial.available()){
     ch=Serial.read();
-  }
   
-  do{
-
-  for(int x=0;x<=6;x++){
+  
+  while(1){
+  for(x=0;x<=6;x++){
     if(segment[(int)ch-48][x]==1)
       digitalWrite(x+2,HIGH);
     else
       digitalWrite(x+2,LOW);
+      delay(200);
   }
-  ch=Serial.read();
-
-  }while(ch==-1);
-
+  x=0;
+  if(Serial.available()){
+    ch=Serial.read();
+    break;
+  }
+  }
+}
 }
